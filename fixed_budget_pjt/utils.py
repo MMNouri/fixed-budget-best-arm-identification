3c6f22cfcb7727606e769b1c3e821bda99debfa1
M.Mojtaba Nouri <96395644+MMNouri@users.noreply.github.com>
2024-05-26 00:46:12 +0200

    Update utils.py

--- fixed_budget_pjt/utils.py
+++ fixed_budget_pjt/utils.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import numpy as np
 
 import torch
-from torch._six import inf
+# from torch._six import inf
 
 from fixed_budget_pjt.config import ENV
 from fixed_budget_pjt.model import PolicyNet
